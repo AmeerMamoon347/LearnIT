412c51979a253fb101b3a6332a9ea32aab389a4f
AmeerMamoon347 <80147222+AmeerMamoon347@users.noreply.github.com>
2021-03-06 19:54:16 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -3,7 +3,8 @@ package com.itbulls.learnit.ameermamoon;
 public class Loops {
 
 	public static void main(String[] args) {
-		 System.out.println("while loop: ");
+		System.out.println("Changes in repository"); 
+		System.out.println("while loop: ");
 		int i = 0;
 		while(i<5) {
 			System.out.println(i++);
